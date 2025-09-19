892d25ce34042d0a8e2260f6a61a37c7a39f657d
sarojgumanju <sarojgumanju107@gmail.com>
2025-09-19 12:44:53 +0545

    styling

---
+++ 4.3.AirbnbTailwind/routes/userRouter.js
@@ -0,0 +1,16 @@
+// core module
+const path = require('path');
+
+// external module
+const express = require ('express');
+const userRouter = express.Router();
+
+// local module
+const rootDir = require("../utils/pathUtil");
+
+userRouter.get("/", (req, res, next) => {
+    // res.sendFile(path.join(__dirname, '../', 'views/home.html'));
+    res.sendFile(path.join(rootDir, 'views/home.html'));
+});
+
+module.exports = userRouter;
