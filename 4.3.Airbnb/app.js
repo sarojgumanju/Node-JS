d6df1a887fecbd993ea43e2847aaf57774a89fb7
sarojgumanju <sarojgumanju107@gmail.com>
2025-09-19 08:05:37 +0545

    path route view

--- 4.3.Airbnb/app.js
+++ 4.3.Airbnb/app.js
@@ -7,16 +7,18 @@ const express = require("express");
 // local module
 const userRouter = require("./routes/userRouter");
 const hostRouter = require("./routes/hostRouter");
+const rootDir = require("./utils/pathUtil")
 
 // app for express
 const app = express();
 
+
 // middleware
-app.use(express.urlencoded());
+app.use(express.urlencoded()); // parsing body
 app.use(userRouter);
 app.use("/host", hostRouter);
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views/404.html'));
 })
 
 
--- 4.3.Airbnb/routes/hostRouter.js
+++ 4.3.Airbnb/routes/hostRouter.js
@@ -5,14 +5,19 @@ const path = require('path');
 const express = require("express");
 const hostRouter = express.Router();
 
+// local module
+const rootDir = require("../utils/pathUtil");
 
 hostRouter.get("/add-home", (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views/addHome.html'));
+    // res.sendFile(path.join(__dirname, '../', 'views/addHome.html'));
+    // filehelper
+    res.sendFile(path.join(rootDir, 'views/addHome.html'));
 });
 
 hostRouter.post("/add-home", (req, res, next) => {
     console.log(req.body);
-    res.sendFile(path.join(__dirname, '../', 'views/homeAdded.html'));
+    // res.sendFile(path.join(__dirname, '../', 'views/homeAdded.html'));
+    res.sendFile(path.join(rootDir, 'views/homeAdded.html'));
 });
 
 module.exports = hostRouter;
