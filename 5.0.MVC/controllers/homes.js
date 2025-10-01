cc953dc2fa9116a7c2d170312c3ccb5c03a608c6
sarojgumanju <sarojgumanju107@gmail.com>
2025-10-01 23:59:06 +0545

    reading and writig from and to a file

--- 5.0.MVC/controllers/homes.js
+++ 5.0.MVC/controllers/homes.js
@@ -1,4 +1,5 @@
-const registeredHomes = [];
+const Home = require("../models/home");
+
 
 //add home page
 const getAddHome = (req, res, next) => {
@@ -7,18 +8,26 @@ const getAddHome = (req, res, next) => {
 
 // home added page
 const postAddHome = (req, res, next) => {
-  console.log('Home Registration successful for:', req.body, req.body.houseName);
-  // registeredHomes.push({houseName: req.body.houseName});
-  registeredHomes.push(req.body);
+  // console.log('Home Registration successful for:', req.body, req.body.houseName);
+
+  const { houseName, price, location, rating, photoUrl } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl);
+  home.save();
   res.render('homeAdded', {pageTitle: 'Home Added Successfully', currentPage: 'homeAdded'});
 }
 
 
 // home page
 const getHomes = (req, res, next) => {
-  console.log(registeredHomes);
-  res.render('home', {registeredHomes: registeredHomes, pageTitle: 'airbnb Home', currentPage: 'Home'});
+  Home.fetchAll((registeredHomes) => 
+    res.render("home", {
+      registeredHomes: registeredHomes,
+      pageTitle: 'airbnb Home',
+      currentPage: "Home"
+    })
+  )
 }
+ 
 
 module.exports = {getAddHome, postAddHome, getHomes};
 
---
+++ 5.0.MVC/models/home.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/pathUtil')
+
+// fake database
+let registeredHomes = [];
+
+module.exports = class Home {
+    constructor(houseName, price, location, rating, photoUrl){
+        this.houseName = houseName;
+        this.price = price;
+        this.location = location;
+        this.rating = rating;
+        this.photoUrl = photoUrl;
+    }
+
+    save(){
+        registeredHomes.push(this);
+        const filePath = path.join(rootDir, 'data', 'homes.json');
+        console.log("this is file path" + filePath);
+        fs.writeFile(filePath, JSON.stringify(registeredHomes), (err) => {
+            console.log("Error check: "); 
+        })
+    }
+
+    static fetchAll(callback){
+        const filePath = path.join(rootDir, "data", "homes.json");
+        fs.readFile(filePath, (err, data) => {
+            console.log("file read: ", err, data);
+            callback(!err? JSON.parse(data) : []);
+        });
+    }
+}
