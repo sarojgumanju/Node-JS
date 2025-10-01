43dc145888b7d408f9cd48989a1882ab258eefd4
sarojgumanju <sarojgumanju107@gmail.com>
2025-10-02 00:14:35 +0545

    some changes in save and fetchAll

--- 5.0.MVC/models/home.js
+++ 5.0.MVC/models/home.js
@@ -1,33 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-const rootDir = require('../utils/pathUtil')
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
 
 // fake database
 let registeredHomes = [];
 
 module.exports = class Home {
-    constructor(houseName, price, location, rating, photoUrl){
-        this.houseName = houseName;
-        this.price = price;
-        this.location = location;
-        this.rating = rating;
-        this.photoUrl = photoUrl;
-    }
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
 
-    save(){
-        registeredHomes.push(this);
-        const filePath = path.join(rootDir, 'data', 'homes.json');
-        console.log("this is file path" + filePath);
-        fs.writeFile(filePath, JSON.stringify(registeredHomes), (err) => {
-            console.log("Error check: "); 
-        })
-    }
+  save() {
+    Home.fetchAll((registeredHomes) => {
+      registeredHomes.push(this);
+      const filePath = path.join(rootDir, "data", "homes.json");
+      console.log("this is file path" + filePath);
+      fs.writeFile(filePath, JSON.stringify(registeredHomes), (err) => {
+        console.log("Error check: ");
+      });
+    });
+  }
 
-    static fetchAll(callback){
-        const filePath = path.join(rootDir, "data", "homes.json");
-        fs.readFile(filePath, (err, data) => {
-            console.log("file read: ", err, data);
-            callback(!err? JSON.parse(data) : []);
-        });
-    }
-}
+  static fetchAll(callback) {
+    const filePath = path.join(rootDir, "data", "homes.json");
+    fs.readFile(filePath, (err, data) => {
+      // console.log("file read: ", err, data);
+      callback(!err ? JSON.parse(data) : []);
+    });
+  }
+};
