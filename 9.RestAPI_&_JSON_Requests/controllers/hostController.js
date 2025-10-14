53b38accfd6e8ee03e0ad9428139ab20094b4060
sarojgumanju <sarojgumanju107@gmail.com>
2025-10-14 21:58:13 +0545

    Rest API

---
+++ 9.RestAPI_&_JSON_Requests/controllers/hostController.js
@@ -0,0 +1,134 @@
+const Home = require("../models/home");
+const fs = require("fs");
+
+// ------------------------- get Add Home -----------------------------
+const getAddHome = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+    isLoggedIn: req.isLoggedIn,
+    user: req.session.user,
+  });
+};
+ 
+
+// ------------------------- get Host Home ------------------------------
+const getHostHomes = (req, res, next) => {
+  Home.find().then((registeredHomes) => {
+    res.render("host/host-home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Host Homes List",
+      currentPage: "host-homes",
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+    })
+  })
+};
+
+
+// ----------------------------- post Add Home -----------------------------
+const postAddHome = (req, res, next) => {
+  const { houseName, price, location, rating, description } = req.body;
+ // console.log(req.body);
+  // console.log(req.file);
+
+ if (!req.file) {
+    return res.status(422).send("No image provided");
+  }
+
+ const photo = req.file.path;
+ console.log("This is the path of Photo: ", photo);
+
+  const home = new Home({
+    houseName,
+    price,
+    location,
+    rating,
+    photo,
+    description,
+  });
+  home.save().then(() => {
+    console.log("Home Saved successfully");
+  });
+
+  res.redirect("/host/host-home-list");
+};
+
+
+// ------------------------------- get Edit Home ------------------------------
+const getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editing = req.query.editing === "true";
+
+  Home.findById(homeId).then(home => {
+    if (!home) {
+      console.log("Home not found for editing.");
+      return res.redirect("/host/host-home-list");
+    }
+
+    console.log(homeId, editing, home);
+    res.render("host/edit-home", {
+      home: home,
+      pageTitle: "Edit your Home",
+      currentPage: "host-homes",
+      editing: editing,
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+    });
+  });
+};
+
+
+// ---------------------------- post Edit Home -----------------------------
+const postEditHome = (req, res, next) => {
+  const { id, houseName, price, location, rating, description } =
+    req.body;
+    
+  Home.findById(id)
+    .then((home) => {
+      home.houseName = houseName;
+      home.price = price;
+      home.location = location;
+      home.rating = rating;
+      home.description = description;
+
+      if (req.file) {
+        fs.unlink(home.photo, (err) => {
+          if (err) {
+            console.log("Error while deleting file ", err);
+          }
+        });
+        home.photo = req.file.path;
+      }
+
+      home
+        .save()
+        .then((result) => {
+          console.log("Home updated ", result);
+        })
+        .catch((err) => {
+          console.log("Error while updating ", err);
+        });
+      res.redirect("/host/host-home-list");
+    })
+    .catch((err) => {
+      console.log("Error while finding home ", err);
+    });
+};
+  
+
+// ---------------------------- post Delete Home --------------------------
+const postDeleteHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  console.log("Came to delete ", homeId);
+  Home.findByIdAndDelete(homeId)
+    .then(() => {
+      res.redirect("/host/host-home-list");
+    })
+    .catch((error) => {
+      console.log("Error while deleting ", error);
+    });
+};
+
+module.exports = { getAddHome, getHostHomes, postAddHome, getEditHome, postEditHome, postDeleteHome};
